365ee2818c1f356ce1160d794ae7407dcc1b19fd
nortrev02 <nortrevor02@gmail.com>
2022-10-12 23:47:05 -0500

    Changes to repo structure

---
+++ TN Assignment 2/TN 303 Assignment 02.cpp
@@ -0,0 +1,10 @@
+// TN 303 Assignment 02.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
